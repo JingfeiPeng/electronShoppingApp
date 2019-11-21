b93c185956663e17460f82eec7f789df157bff1c
Jeff Peng <Jeff.Peng@capco.com>
2019-11-21 16:35:32 -0500

    Main window finished

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const electron =  require('electron');
 const url = require('url');
 const path = require('path')
 
-const { app, BrowserWindow, Menu } =  electron;
+const { app, BrowserWindow, Menu, ipcMain } =  electron;
 
 let mainWindow;
 let addWindow;
+
+// catch item:add
+ipcMain.on("item:add", function(e, item){
+    console.log(item)
+    mainWindow.webContents.send("item:add", item);
+    addWindow.close();
+})
 
 // create menu template
 const mainMenuTemplate = [
@@ -18,7 +25,13 @@ const mainMenuTemplate = [
                     createAddWindow()
                 }
             },
-            { label:"Clear Items" },
+            { 
+                label:"Clear Items",
+                click(){
+                    // send an event to mainWindow.html
+                    mainWindow.webContents.send('item:clear');
+                }
+             },
             {
                 label: "Quit",
                 accelerator: process.platform == 'darwin' ? "Command+Q": "Ctrl+Q",
@@ -59,12 +72,17 @@ if (process.env.NODE_env !== 'production'){
 // listen for app to be ready
 app.on('ready', function(){
     // create new window
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        //The lines below allow require to be used in html
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
     // load HTML into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
-        slashes: true
+        slashes: true,
     }));
     // Quit App when closed
     mainWindow.on('closed', function(){
@@ -82,7 +100,11 @@ function createAddWindow(){
     addWindow = new BrowserWindow({
         width:300,
         height:200,
-        title: "Add Shopping List item"
+        title: "Add Shopping List item",
+        //The lines below solved the issue
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
     // load HTML into window
     addWindow.loadURL(url.format({
